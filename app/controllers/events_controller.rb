6e0d22c48f5c7887a7fabc0992645135061f84f6
riley <riley@hidinginthevoid.com>
2024-10-11 16:49:59 -0700

    [Event] Checking wrong value
    We really needed to check the membership_number value NOT the membership_code

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,8 +1,14 @@
 class EventsController < ApplicationController
   def clear_discount_from_cache
-    if current_user.membership_code == params[:code]
+    if current_user.membership_number == params[:code]
       Rails.cache.delete("eventbrite:event:#{params[:eventbrite_id]}:discounts:#{params[:code]}")
     else
+      Rollbar.warn(
+        'Attempted to clear cache value by user not matching member number',
+        current_user: current_user.id,
+        current_user_membership_number: current_user.membership_number,
+        provided_code: params[:code]
+      )
       head :bad_request
     end
   end
