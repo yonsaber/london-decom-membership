d15fb763ba1b5326fa76f2a1449b0edaea81ce42
riley <riley@hidinginthevoid.com>
2024-10-19 16:26:02 -0700

    [Mailer] If there are no leads fallback email address
    Otherwise we are ending up with Postmark::InvalidEmailRequestError errors in the system

--- app/mailers/leads_mailer.rb
+++ app/mailers/leads_mailer.rb
@@ -2,18 +2,18 @@ class LeadsMailer < ApplicationMailer
   def new_volunteer(volunteer)
     @volunteer = volunteer
     mail(
-      to: volunteer.volunteer_role.lead_emails,
+      to: get_to_email_with_fallback(volunteer),
       from: 'volunteers@londondecom.org',
-      subject: 'New Decom volunteer'
+      subject: "New Decom volunteer for #{volunteer.volunteer_role.name}"
     )
   end
 
   def cancelled_volunteer(volunteer)
     @volunteer = volunteer
     mail(
-      to: volunteer.volunteer_role.lead_emails,
+      to: get_to_email_with_fallback(volunteer),
       from: 'volunteers@londondecom.org',
-      subject: 'Decom volunteer cancellation'
+      subject: "Decom volunteer cancellation for #{volunteer.volunteer_role.name}"
     )
   end
 
@@ -24,5 +24,15 @@ class LeadsMailer < ApplicationMailer
       from: 'volunteers@londondecom.org',
       subject: "You've been made a lead of #{@volunteer.volunteer_role.name} for London Decom"
     )
+  end
+
+  private
+
+  # NOTE: This is a safety check if we don't have a lead for a role then we send the email to volunteers just
+  #       so we don't get flooded with error logs about trying to send emails to no recipients.
+  def get_to_email_with_fallback(volunteer)
+    to_emails = volunteer.volunteer_role.lead_emails
+    to_emails&.push('volunteers@londondecom.org') if to_emails.nil? || to_emails.empty?
+    to_emails
   end
 end
--- spec/features/volunteer_spec.rb
+++ spec/features/volunteer_spec.rb
@@ -133,6 +133,38 @@ RSpec.feature 'Volunteering', type: :feature do
     expect(page).to have_text('You are confirmed as a volunteer')
   end
 
+  scenario 'signing up to volunteer successfully without leads' do
+    create(:volunteer_role, name: 'Ranger', description: 'A description of rangering')
+    stub_eventbrite_event(tickets_sold_for_code: 1)
+    login
+
+    click_link 'Volunteering'
+    expect(page).to have_text('Ranger')
+    click_link 'Volunteer'
+    expect(page).to have_text('A description of rangering')
+    fill_in 'Phone', with: '07777777'
+    fill_in 'Additional comments', with: 'some addition comments'
+    check 'I agree to the Decom Code of Conduct'
+    check "I confirm that I've read the Decom Health and Safety Guidelines"
+    click_button 'Volunteer'
+    expect(page).to have_text("You've signed up to volunteer for")
+    expect(@user.volunteers.count).to eq(1)
+    expect(@user.volunteers.first.phone).to eq('07777777')
+    expect(@user.volunteers.first.additional_comments).to eq('some addition comments')
+
+    open_email('volunteers@londondecom.org')
+    expect(current_email).to have_content('James Darling just volunteered for Ranger')
+
+    expect(page).to have_text('The leads for this role should be in contact with you very soon')
+    volunteer = @user.volunteers.last
+    volunteer.update(state: 'contacted')
+    visit event_volunteering_index_path(:event)
+    expect(page).to have_text('You have been contacted by a lead')
+    volunteer.update(state: 'confirmed')
+    visit event_volunteering_index_path(:event)
+    expect(page).to have_text('You are confirmed as a volunteer')
+  end
+
   scenario 'signing up to limited volunteer successfully and update page for other users' do
     role = create(:volunteer_role, name: 'Limited Role', description: 'Limited Role', available_slots: 1)
     create(:volunteer, volunteer_role: role, lead: true).user
