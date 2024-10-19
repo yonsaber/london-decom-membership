8f03e92b53c509b7bb08404737212036d5832eb6
riley <riley@hidinginthevoid.com>
2024-10-19 16:24:36 -0700

    [Event] Show sold out state for ticket
    When the ticket class sells out, indicate to the user that this is the case

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,7 @@
 class EventsController < ApplicationController
   def clear_discount_from_cache
     if current_user.membership_number == params[:code]
-      Rails.cache.delete("eventbrite:event:#{params[:eid]}:discounts:#{params[:code]}")
+      delete_records_in_cache(params[:eid], params[:code])
       head :ok
     else
       Rollbar.warn(
@@ -12,5 +12,22 @@ class EventsController < ApplicationController
       )
       head :bad_request
     end
+  end
+
+  private
+
+  def delete_records_in_cache(eventbrite_id, code)
+    deleted_records = Rails.cache.delete_multi(
+      %W[eventbrite:event:#{eventbrite_id}:discounts:#{code} eventbrite:event:#{eventbrite_id}:ticketclasses]
+    )
+    return unless deleted_records != 2
+
+    Rollbar.warn(
+      'Tried to clear discounts and ticket classes, had an issue doing so',
+      records_cleared: "#{deleted_records} of an expected 2",
+      current_user: current_user.id,
+      current_user_membership_number: current_user.membership_number,
+      provided_code: params[:code]
+    )
   end
 end
