268a185ad4c0a3beffe8ae303a03826273852c5e
riley <riley@hidinginthevoid.com>
2024-10-27 14:47:34 -0700

    [LI] CSV generator for all low income requests
    Requested by the ticket team to make contacting people easier

--- app/models/low_income_request.rb
+++ app/models/low_income_request.rb
@@ -10,4 +10,23 @@ class LowIncomeRequest < ApplicationRecord
   def reject!
     update(status: 'rejected')
   end
+
+  def self.to_csv
+    require 'csv'
+
+    CSV.generate(headers: true) do |csv|
+      csv << %w[Name Email LI\ Request\ Status Ticket\ Bought Created\ At Last\ Updated\ At]
+
+      all.each do |lir|
+        csv << [
+          lir.user.name,
+          lir.user.email,
+          lir.status&.capitalize || 'No Current Status',
+          lir.user.ticket_bought ? 'Yes' : 'No',
+          lir.created_at.to_fs(:decom_standard),
+          lir.updated_at.to_fs(:decom_standard)
+        ]
+      end
+    end
+  end
 end
