6251036edd4615ae8569fdfb74b5dd47ab6319c8
riley <riley@hidinginthevoid.com>
2024-11-12 19:34:53 -0800

    Remove temp fix for sold out check

--- spec/features/tickets_spec.rb
+++ spec/features/tickets_spec.rb
@@ -68,10 +68,6 @@ RSpec.feature 'Tickets', type: :feature do
     login
 
     expect(page).to_not have_text('Buy Ticket')
-    if Time.zone.now.before?(Time.parse('2024-11-04 19:00:00').change(offset: 0))
-      expect(page).to have_text('Tickets are currently sold out')
-    else
-      expect(page).to have_text('Tickets are sold out')
-    end
+    expect(page).to have_text('Tickets are sold out')
   end
 end
