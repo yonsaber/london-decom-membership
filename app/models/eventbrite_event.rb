e8b750a02fbb93692bcc72a3fb3d39e2daf94415
riley <riley@hidinginthevoid.com>
2025-10-10 10:21:17 -0700

    [Event] Change message if ticket class not on sale
    Allows for early access to be set, but show the tickets are not on sale yet, instead of  immediately showing sold out

--- app/models/eventbrite_event.rb
+++ app/models/eventbrite_event.rb
@@ -25,7 +25,7 @@ class EventbriteEvent
     until finished
       response = HTTP.get(
         "https://www.eventbriteapi.com/v3/organizations/#{organization_id}/discounts/" \
-        "?scope=event&event_id=#{eventbrite_id}&token=#{eventbrite_token}&page_size=200&page=#{page}"
+          "?scope=event&event_id=#{eventbrite_id}&token=#{eventbrite_token}&page_size=200&page=#{page}"
       )
       if JSON.parse(response)['error'] == 'BAD_PAGE'
         finished = true
@@ -43,7 +43,7 @@ class EventbriteEvent
     discounts = Rails.cache.fetch("eventbrite:event:#{eventbrite_id}:discounts:#{code}", expires_in: 30.minutes) do
       response = HTTP.get(
         "https://www.eventbriteapi.com/v3/organizations/#{organization_id}/discounts/" \
-        "?scope=event&code=#{code}&event_id=#{eventbrite_id}&token=#{eventbrite_token}"
+          "?scope=event&code=#{code}&event_id=#{eventbrite_id}&token=#{eventbrite_token}"
       )
       JSON.parse(response)['discounts']
     end
@@ -53,18 +53,14 @@ class EventbriteEvent
     @discount_code = discounts[0]
   end
 
+  def ticket_class_not_on_sale?(code)
+    check_user_ticket_classes_by_statuses(code, 'NOT_YET_ON_SALE').empty?
+  rescue EventbriteDiscountCodeNotFound
+    false
+  end
+
   def ticket_class_sold_out?(code)
-    user_discount_code = discount_code(code)
-    no_tickets_to_buy = (user_discount_code['quantity_available'] - user_discount_code['quantity_sold']).zero?
-
-    # NOTE: No point checking if the tickets are on sale if there are no tickets for the user to buy / they have
-    #       have already bought their tickets
-    return false if no_tickets_to_buy
-
-    user_ticket_class_ids = user_discount_code['ticket_class_ids']
-    available_ticket_classes = find_user_ticket_classes_by_status(user_ticket_class_ids, 'AVAILABLE')
-
-    available_ticket_classes.empty?
+    check_user_ticket_classes_by_statuses(code, 'AVAILABLE').empty?
   rescue EventbriteDiscountCodeNotFound
     false
   end
@@ -104,6 +100,17 @@ class EventbriteEvent
     user = LowIncomeCode.find_by(code:)&.low_income_request&.user if user.nil?
     user = DirectSaleCode.find_by(code:).user if user.nil?
     user
+  end
+
+  def check_user_ticket_classes_by_statuses(code, status)
+    user_discount_code = discount_code(code)
+    no_tickets_to_buy = (user_discount_code['quantity_available'] - user_discount_code['quantity_sold']).zero?
+
+    return [] if no_tickets_to_buy
+
+    user_ticket_class_ids = user_discount_code['ticket_class_ids']
+
+    find_user_ticket_classes_by_status(user_ticket_class_ids, status)
   end
 
   def fetch_ticket_classes
