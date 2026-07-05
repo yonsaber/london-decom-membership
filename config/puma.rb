2e1724837efd9b7ad3d69bf26987b5843430e7da
riley <riley@hidinginthevoid.com>
2026-07-05 01:24:49 -0700

    [System] Upgrade to Rails 8.1

--- config/puma.rb
+++ config/puma.rb
@@ -20,25 +20,18 @@
 # Any libraries that use a connection pool or another resource pool should
 # be configured to provide at least as many connections as the number of
 # threads. This includes Active Record's `pool` parameter in `database.yml`.
-ENV.fetch('WEB_CONCURRENCY', 2)
 threads_count = ENV.fetch('RAILS_MAX_THREADS', 5)
 threads threads_count, threads_count
 
-preload_app!
-
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 port ENV.fetch('PORT', 3000)
-environment ENV.fetch('RACK_ENV', 'development')
 
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
 
+# Run the Solid Queue supervisor inside of Puma for single-server deployments.
+plugin :solid_queue if ENV['SOLID_QUEUE_IN_PUMA']
+
 # Specify the PID file. Defaults to tmp/pids/server.pid in development.
 # In other environments, only set the PID file if requested.
 pidfile ENV['PIDFILE'] if ENV['PIDFILE']
-
-on_worker_boot do
-  # Worker specific setup for Rails 4.1+
-  # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
-  ActiveRecord::Base.establish_connection
-end
