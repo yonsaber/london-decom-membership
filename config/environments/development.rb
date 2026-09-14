9325e4385b51a072f950e70f11752fd833d7c16f
riley <riley@hidinginthevoid.com>
2026-09-14 10:21:48 -0700

    [System] Fix small typo and remove dev config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -101,6 +101,4 @@ Rails.application.configure do
   logger = ActiveSupport::Logger.new($stdout)
   logger.formatter = config.log_formatter
   config.logger = ActiveSupport::TaggedLogging.new(logger)
-
-  config.hosts << 'members.lab.hidinginthevoid.com'
 end
